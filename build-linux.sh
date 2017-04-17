1b2b627dbf69267f1b64e452ce392a261b108ed6
Jon Schoning <jonschoning@gmail.com>
2017-04-17 17:03:44 -0500

    add scripts

---
+++ build-linux.sh
@@ -0,0 +1,2 @@
+dotnet build --framework netstandard1.5 Unions/Unions.csproj
+dotnet build --framework netcoreapp1.1 Unions.Tests/Unions.Tests.csproj
---
+++ build-win.bat
@@ -0,0 +1,2 @@
+dotnet build --framework net452 Unions/Unions.csproj
+dotnet build --framework net452 Unions.Tests/Unions.Tests.csproj
