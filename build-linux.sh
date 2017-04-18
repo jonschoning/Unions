85ff87f0bcd48e39af08aa8b76232f89e7cfa26c
Jon Schoning <jonschoning@gmail.com>
2017-04-17 23:31:41 -0500

    add Serializeable

--- build-linux.sh
+++ build-linux.sh
@@ -1,3 +1,3 @@
 dotnet restore
-dotnet build --framework netstandard1.5 Unions/Unions.csproj
+dotnet build --framework netstandard1.4 Unions/Unions.csproj
 dotnet build --framework netcoreapp1.1 Unions.Tests/Unions.Tests.csproj
